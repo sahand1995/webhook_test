f21e7f83c5d6ca40d1c6f19d87cd37e8f4ee74f7
sahand <jalali.sahand1995@gmail.com>
2024-11-25 13:23:51 +0330

    remove extra parameters

--- first_file.py
+++ first_file.py
@@ -2,15 +2,4 @@ def calculate_sum(a, b):
     result = a + b
     return result
 
-# Example usage
-num1 = 5
-num2 = 3
-sum_result = calculate_sum(num1, num2)
-print(f"The sum of {num1} and {num2} is: {sum_result}")print("hello world")
-print("and another line added")
 
-
-x = 1 + 2 
-y = 5 
-
-result = x * y
